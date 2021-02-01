c4d0cb882d2b2dc33b083a25a0ce38280c0672dc
Alexis Saettler <alexis@saettler.org>
2021-02-02 00:06:51 +0100

    ci: fix script

--- commit-changelog.sh
+++ commit-changelog.sh
@@ -1,20 +1,54 @@
 #!/bin/bash
 
+repo=asbiin/semantic-release-test
+base=master
+
 newbranch=$(date +"%Y-%m-%d")-update-changelog
 
-# Clean
-git branch -D $newbranch || true
-git branch -D $origin/$newbranch || true
+content=$(base64 -w 0 CHANGELOG.md)
 
-# Create new branch
-git checkout -b $newbranch
+# Test if branch already exists
+test=$(curl -fsSL \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/$repo/git/ref/heads/$newbranch 2> /dev/null)
 
-# Update the new branch
-git add "CHANGELOG.md"
-git commit -m "chore(changelog): Update Changelog"
+if [ $? = 0 ]; then
+  # Delete previous branch
+  curl -sSL \
+    -X DELETE \
+    -H "Accept: application/vnd.github.v3+json" \
+    -H "Authorization: token $GITHUB_TOKEN" \
+    https://api.github.com/repos/$repo/git/refs/heads/$newbranch > /dev/null
+fi
 
-# Push it to remote
-git push $origin $newbranch
+# Create a new branch
+curl -sSL \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/$repo/git/refs \
+  -d "{\"ref\":\"refs/heads/$newbranch\",\"sha\":\"$GITHUB_SHA\"}" > /dev/null
+
+# Get changelog sha
+sha=$(curl -sSL \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/$repo/contents/CHANGELOG.md \
+  | jq '.sha')
+
+# Upload file content
+curl -sSL \
+  -X PUT \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/$repo/contents/CHANGELOG.md \
+  -d "{\"message\":\"chore(changelog): Update Changelog\",\"sha\":$sha,\"branch\":\"$newbranch\",\"content\":\"$content\"}" > /dev/null
 
 # Create a pull request
-gh pr create --title "chore(changelog): Update Changelog" --body "Update changelog with last data"
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/$repo/pulls \
+  -d "{\"head\":\"$newbranch\",\"base\":\"$base\",\"title\":\"chore(changelog): Update Changelog\"}" > /dev/null
